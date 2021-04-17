6de3f8765de539c6c9a38be4ae44402fa7ecbb4a
ryfprivate <ryfprivate@outlook.com>
2021-04-18 00:04:35 +1000

    Basic bridge scene with sample/freq band audio cube visualisers

---
+++ Assets/Scripts/AudioPeer.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPeer : MonoBehaviour
+{
+    AudioSource audioSource;
+    public static float[] samples = new float[512];
+    // Normalized
+    public static float[] audioBand = new float[8];
+    public static float[] audioBandBuffer = new float[8];
+    public static float amplitude, amplitudeBuffer;
+
+    private float[] freqBand = new float[8];
+    private float[] bandBuffer = new float[8];
+    private float[] bufferDecrease = new float[8];
+
+    private float[] freqBandMax = new float[8];
+    private float amplitudeMax;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        GetSpectrumAudioSource();
+        MakeFrequencyBands();
+        BandBuffer();
+        CreateAudioBands();
+        GetAmplitude();
+    }
+
+    void GetAmplitude()
+    {
+        float currentAmplitude = 0;
+        float currentAmplitudeBuffer = 0;
+
+        for (int i = 0; i < 8; i++)
+        {
+            currentAmplitude += audioBand[i];
+            currentAmplitudeBuffer += audioBandBuffer[i];
+        }
+
+        if (currentAmplitude > amplitudeMax)
+        {
+            amplitudeMax = currentAmplitude;
+        }
+
+        amplitude = currentAmplitude / amplitudeMax;
+        amplitudeBuffer = currentAmplitudeBuffer / amplitudeMax;
+    }
+
+    void CreateAudioBands()
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            if (freqBand[i] > freqBandMax[i])
+            {
+                freqBandMax[i] = freqBand[i];
+            }
+            audioBand[i] = freqBand[i] / freqBandMax[i];
+            audioBandBuffer[i] = bandBuffer[i] / freqBandMax[i];
+        }
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        audioSource.GetSpectrumData(samples, 0, FFTWindow.Blackman);
+    }
+
+    void BandBuffer()
+    {
+        for (int g = 0; g < 8; ++g)
+        {
+            if (freqBand[g] > bandBuffer[g])
+            {
+                bandBuffer[g] = freqBand[g];
+                bufferDecrease[g] = 0.005f;
+            }
+            if (freqBand[g] < bandBuffer[g])
+            {
+                bandBuffer[g] -= bufferDecrease[g];
+                bufferDecrease[g] *= 1.2f;
+            }
+        }
+    }
+
+    void MakeFrequencyBands()
+    {
+        // Sub-bass: 20 - 60
+        // Bass: 60 - 250
+        // 250 - 500
+        // 500 - 2000
+        // 2000 - 4000
+        // 4000 - 6000
+        // 6000 - 20000
+        // samples per bank (if theres 43 hz per sample)
+        // 0: 2 samples = 86 hz (0 - 86)
+        // 1: 4 = 172 (87 - 258)
+        // 2: 8 = 344 (259 - 602)
+        // 3: 16 = 688 (603-1290)
+        // 4: 32 = 1376
+        // ... 7: 256 = 11008 (10923 - 21930)
+        // 510 samples
+
+        int count = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            float average = 0;
+            int sampleCount = (int)Mathf.Pow(2, i) * 2;
+
+            for (int j = 0; j < sampleCount; j++)
+            {
+                average += samples[count] * (count + 1);
+                count++;
+            }
+
+            average /= count;
+
+            freqBand[i] = average * 10;
+
+        }
+    }
+}
---
+++ Assets/Scripts/InstantiateCubes.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateCubes : MonoBehaviour
+{
+    public GameObject cubePrefab;
+    public float maxScale;
+    public static int numCubes = 512;
+    private GameObject[] sampleCubes = new GameObject[numCubes];
+
+    void Start()
+    {
+        for (int i = 0; i < numCubes; i++)
+        {
+            GameObject cubeInstance = (GameObject)Instantiate(cubePrefab);
+            cubeInstance.transform.position = this.transform.position;
+            cubeInstance.transform.parent = this.transform;
+            cubeInstance.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -(360f / numCubes) * i, 0);
+            cubeInstance.transform.position = Vector3.forward * 40;
+            sampleCubes[i] = cubeInstance;
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < numCubes; i++)
+        {
+            if (sampleCubes != null)
+            {
+                Vector3 newScale = new Vector3(transform.localScale.x, (AudioPeer.samples[i] * maxScale), transform.localScale.z);
+
+                sampleCubes[i].transform.localScale = newScale;
+                sampleCubes[i].transform.position = new Vector3(sampleCubes[i].transform.position.x, 0 + newScale.y / 2, sampleCubes[i].transform.position.z);
+
+            }
+        }
+    }
+}
