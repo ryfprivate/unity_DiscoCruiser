288b69327c541d89f784dad8438de87788a693b2
ryfprivate <ryfprivate@outlook.com>
2021-04-18 14:16:35 +1000

    Added car and post processing effect

---
+++ Assets/Scripts/Car.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    public GameObject[] wheels;
+
+    void Update()
+    {
+        for (int i = 0; i < wheels.Length; i++)
+        {
+            wheels[i].transform.Rotate(1f, 0, 0);
+        }
+    }
+}
